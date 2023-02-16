bb6274828c44e8d99466a9114db6f6629d3411f8
TOEI <chonchalit.t@kerryexpress.com>
2023-02-16 20:38:58 +0700

    init project

---
+++ app/src/main/java/th/co/toei/pokedex_dragger_hilt_modularization/MainActivity.kt
@@ -0,0 +1,11 @@
+package th.co.toei.pokedex_dragger_hilt_modularization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
